22461767ef844e4423b48c8b8a898bda5724c142
phum <aekkaratthaphum@outlook.co.th>
2023-05-14 04:43:38 +0700

    done

--- Program.cs
+++ Program.cs
@@ -2,58 +2,83 @@
 
 class Program{
     static void Main(string[] args){
-        Console.Write("Enter DNA :");
-        string DNAInput = Console.ReadLine();
+        
 
 
         bool isWorking = true;
+       
         while(isWorking){
-        if(IsValidSequence(DNAInput)){
-                Console.WriteLine("Replicated half DNA sequence : " + DNAInput);
-                while(true){
-                    Console.Write("Do you want to replicate it ? (Y/N) :");
-                    string userInput1 = Console.ReadLine();
-                    if(userInput1 == "Y"){
-                        Console.WriteLine("Replicated half DNA sequence : " + ReplicateSeqeunce(DNAInput));
-                        while(isWorking){
-                            Console.Write("Do you want to process another sequence ? (Y/N) :");
-                            string userInput3 = Console.ReadLine();
-                            if(userInput3 == "Y"){
-                                break;
-                            }else if(userInput3 == "N"){
-                                isWorking = false;
-                                break;
-                            }else{
-                                Console.WriteLine("Please input Y or N.");
-                                continue;
+             bool isWorking2 = true;
+             bool isWorking3 = true;
+            Console.Write("Enter DNA :");
+            string DNAInput = Console.ReadLine();
+            
+            if(IsValidSequence(DNAInput)){
+                    Console.WriteLine("Current half DNA sequence : " + DNAInput);
+                    while(isWorking3){
+                        Console.Write("Do you want to replicate it ? (Y/N) :");
+                        string userInput = Console.ReadLine();
+                        if(userInput == "Y"){
+                            Console.WriteLine("Replicated half DNA sequence : " + ReplicateSeqeunce(DNAInput));
+                            while(isWorking2){
+                                Console.Write("Do you want to process another sequence ? (Y/N) :");
+                                userInput = Console.ReadLine();
+                                if(userInput == "Y"){
+                                    isWorking2 = false;
+                                    isWorking3 = false;
+                                    break;
+                                }else if(userInput == "N"){
+                                    isWorking = false;
+                                    isWorking3 = false;
+                                    break;
+                                }else{
+                                    Console.WriteLine("Please input Y or N.");
+                                    continue;
+                                }
                             }
+                            
+                        }else if(userInput =="N"){
+    
+                                 while(isWorking2){
+                                    Console.Write("Do you want to process another sequence ? (Y/N) :");
+                                    userInput = Console.ReadLine();
+                                    if(userInput == "Y"){
+                                        isWorking2 = false;
+                                        isWorking3 = false;
+                                        break;
+                                    }else if(userInput == "N"){
+                                        isWorking = false;
+                                        isWorking3 = false;
+                                        break;
+                                    }else{
+                                        Console.WriteLine("Please input Y or N.");
+                                        continue;
+                                    }
+                                 }
+                        }else{
+                            Console.WriteLine("Please input Y or N.");
+                            continue;
                         }
-                        
-                    }else if(userInput1 =="N"){
-                        isWorking = false;
-                        break;
-                    }else{
-                        Console.WriteLine("Please input Y or N.");
-                        continue;
                     }
-                }
-        }else{
-                Console.WriteLine("That half DNA sequence is invalid.");
-                while(isWorking){
-                    Console.Write("Do you want to process another sequence ? (Y/N) :");
-                    string userInput2 = Console.ReadLine();
-                    if(userInput2 == "Y"){
-                        break;
-                    }else if(userInput2 =="N"){
-                        isWorking = false;
-                        break;
-                    }else{
-                        Console.WriteLine("Please input Y or N.");
-                        continue;
+            }else{
+                    Console.WriteLine("That half DNA sequence is invalid.");
+                    while(isWorking2){
+                        Console.Write("Do you want to process another sequence ? (Y/N) :");
+                        string userInput = Console.ReadLine();
+                        if(userInput == "Y"){
+                            isWorking2 = false;
+                            break;
+                        }else if(userInput =="N"){
+                            isWorking = false;
+                            isWorking3 = false;
+                            break;
+                        }else{
+                            Console.WriteLine("Please input Y or N.");
+                            continue;
+                        }
                     }
                 }
             }
-        }
 
 
     }
